e233484b36e113844b3318759283e2e7ac263e13
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2025-05-09 18:43:39 -0300

    initial project

---
+++ MTS/MTSTests/MTSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MTSTests.swift
+//  MTSTests
+//
+//  Created by Giovanne Bressam on 09/05/25.
+//
+
+import Testing
+@testable import MTS
+
+struct MTSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
