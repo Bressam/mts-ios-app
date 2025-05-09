06832c3bd7af587fc3f0016d1264b8eec9a2d8ed
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2025-05-09 19:33:08 -0300

    implements CoordinatorSpy on testing target

---
+++ Modules/CoordinatorKit/CoordinatorKit/Testing/Implementation/Sources/CoordinatorSpy.swift
@@ -0,0 +1,35 @@
+//
+//  StubService.swift
+//  SignInTesting
+//
+//  Created by Giovanne Bressam on 09/05/25.
+//  Copyright © 2024 dev.bressam. All rights reserved.
+//
+
+import Foundation
+import CoordinatorKitInterface
+
+/// Spy implementation for `CoordinatorProtocol`. Notifies by closure any protocol method called 
+public final class CoordinatorSpy: CoordinatorProtocol {
+    public var navigationController: DefaultNavigationController
+    public var didStart: (() -> Void)?
+    public var didDisismiss: (() -> Void)?
+    public var didStartChildFlow: ((CoordinatorProtocol) -> Void)?
+
+    public init(navigationController: DefaultNavigationController? = nil) {
+        self.navigationController = navigationController ?? .init()
+    }
+    
+
+    public func start() {
+        didStart?()
+    }
+
+    public func dismiss(animated: Bool) {
+        didDisismiss?()
+    }
+
+    public func startChildFlow(with coordinator: CoordinatorProtocol, animated: Bool) {
+        didStartChildFlow?(coordinator)
+    }
+}
