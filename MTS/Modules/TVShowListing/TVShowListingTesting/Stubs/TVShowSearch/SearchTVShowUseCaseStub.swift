ecaa85af48cf032f22c9ec61ceb244c5108a4d12
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2025-05-11 12:41:45 -0300

    creates SearchTVShowUseCaseStub

---
+++ MTS/Modules/TVShowListing/TVShowListingTesting/Stubs/TVShowSearch/SearchTVShowUseCaseStub.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTVShowUseCaseStub.swift
+//  TVShowListing
+//
+//  Created by Giovanne Bressam on 11/05/25.
+//
+
+import Foundation
+import TVShowListingFeatureDomain
+
+public final class SearchTVShowUseCaseStub: SearchTVShowsUseCaseProtocol {
+    public init() {}
+
+    public func execute(query: String) async throws -> [TVShowListingFeatureDomain.TVShowSearchResult] {
+        return TVShow.mock.map({ TVShowSearchResult(score: 0.0, show: $0) })
+    }
+}
