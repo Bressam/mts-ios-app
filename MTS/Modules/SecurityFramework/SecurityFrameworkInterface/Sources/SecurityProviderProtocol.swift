60d5b57def215b26ba0befbaf895ea1f5755fd2c
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2025-05-11 18:07:45 -0300

    creates SecurityProviderProtocol

---
+++ MTS/Modules/SecurityFramework/SecurityFrameworkInterface/Sources/SecurityProviderProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  SecurityProviderProtocol.swift
+//  SecurityFramework
+//
+//  Created by Giovanne Bressam on 11/05/25.
+//
+
+public protocol SecurityProviderProtocol {
+    /// Requests user authentication with PIN or biometric (if enabled).
+    /// - Returns: A boolean indicating if authentication was successful.
+    func requestAuthentication() async -> Bool
+    
+    /// Configures the user's PIN for authentication.
+    /// - Parameter pin: The user's chosen PIN number.
+    func setPIN(_ pin: String)
+    
+    /// Enables or disables biometric authentication (FaceID/TouchID).
+    /// - Parameter enabled: A boolean value.
+    func setBiometricAuthenticationEnabled(_ enabled: Bool)
+}
